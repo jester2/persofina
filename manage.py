d66268c198b8ecfd045097a42d0a32560b351746
Cesar Lucas <jester2@gmail.com>
2016-12-27 15:40:29 -0500

    Added first routes

--- manage.py
+++ manage.py
@@ -2,4 +2,12 @@ from app import create_app
 
 app = create_app()
 
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s!</h1>' % name
+
 app.run()
