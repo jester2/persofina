7abfcc6979836f4e2c04dd94b5400cf6e331c9f2
Cesar Lucas <jester2@gmail.com>
2016-12-28 10:38:00 -0500

    Added notes regarding __main__

--- manage.py
+++ manage.py
@@ -1,13 +1,21 @@
 from app import create_app
+from flask import request
 
 app = create_app()
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	user_agent = request.headers.get('User_Agent')
+	return '<p>Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
 	return '<h1>Hello, %s!</h1>' % name
 
-app.run()
+# The below line is used to ensure that the development
+# web serveris started only when the script is executed
+# directly.  When the script is imported by another script
+# it is assumed that the parent script will launch a different
+# server, so the app.run() call gets skipped
+if __name__ == '__main__':
+	app.run(debug=True)
